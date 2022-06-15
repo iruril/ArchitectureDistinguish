4281c33dca07b16b6e5d91744e8098dd6250e1b5
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-06-15 23:20:21 +0900

    Achtecture Distinguish
    가상환경을 새로 설정한 후, 건축물의 사진에 따라 분류하는 인공지능 작성. MNIST 이미지 분류 문제를 따라해봤던 경험을 살려서 진행해 봤으며, 크기가 다른 모든 이미지를 크롭하고 해상도를 통일해 학습하도록 하였음.

---
+++ ArchtectureDistinguish.py
@@ -0,0 +1,126 @@
+import os
+import zipfile
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+local_zip = 'dataset.zip'
+
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+base_dir = '/tmp'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+train_baroque_dir = os.path.join(train_dir, 'baroque')
+train_gothic_dir = os.path.join(train_dir, 'gothic')
+print(train_baroque_dir)
+print(train_gothic_dir)
+
+validation_baroque_dir = os.path.join(validation_dir, 'baroque')
+validation_gothic_dir = os.path.join(validation_dir, 'gothic')
+print(validation_baroque_dir)
+print(validation_gothic_dir)
+
+train_baroque_fnames = os.listdir( train_baroque_dir )
+train_gothic_fnames = os.listdir( train_gothic_dir )
+
+print(train_baroque_fnames[:5])
+print(train_gothic_fnames[:5])
+
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+nrows, ncols = 4, 4
+pic_index = 0
+
+fig = plt.gcf()
+fig.set_size_inches(ncols*3, nrows*3)
+
+pic_index+=8
+
+next_baroque_pix = [os.path.join(train_baroque_dir, fname)
+                for fname in train_baroque_fnames[ pic_index-8:pic_index]]
+
+next_gothic_pix = [os.path.join(train_gothic_dir, fname)
+                for fname in train_gothic_fnames[ pic_index-8:pic_index]]
+
+for i, img_path in enumerate(next_baroque_pix+next_gothic_pix):
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off')
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation='relu'),
+  tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(optimizer=RMSprop(lr=0.001),
+            loss='binary_crossentropy',
+            metrics = ['accuracy'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                  batch_size=20,
+                                                  class_mode='binary',
+                                                  target_size=(150, 150))
+validation_generator =  test_datagen.flow_from_directory(validation_dir,
+                                                       batch_size=20,
+                                                       class_mode  = 'binary',
+                                                       target_size = (150, 150))
+
+history = model.fit(train_generator,
+                    validation_data=validation_generator,
+                    steps_per_epoch=100,
+                    epochs=100,
+                    validation_steps=20,
+                    verbose=2)
+
+import matplotlib.pyplot as plt
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'go', label='Training Loss')
+plt.plot(epochs, val_loss, 'g', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
